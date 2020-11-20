12d4ff21d0cefc11c563a8ace4c4e5c6cd3d5755
somsomdah <somdah98@gmail.com>
2020-11-20 20:17:22 +0900

    201120

--- _11725_ParentOfTree.py
+++ _11725_ParentOfTree.py
@@ -2,6 +2,42 @@ import sys
 
 n=int(sys.stdin.readline())
 
+adj_list=[[] for _ in range(n+1)]
+
+for _ in range(n-1):
+     v1,v2=map(int,sys.stdin.readline().split())
+     adj_list[v1].append(v2)
+     adj_list[v2].append(v1)
+
+
+parent=[0]*(n+1)
+
+Q=[1]
+
+while Q:
+     v=Q.pop(0)
+     for child in adj_list[v]:
+
+          parent[child]=v
+          adj_list[child].remove(v)
+     Q.extend(adj_list[v])
+
+     #print(Q)
+
+for i in range(2,n+1):
+     print(parent[i])
+          
+
+
+
+
+
+'''
+# 메모리 초과
+import sys
+
+n=int(sys.stdin.readline())
+
 adj_mat=[[0]*(n+1) for _ in range(n+1)]
 
 for _ in range(n-1):
@@ -36,3 +72,4 @@ for i in range(1,n+1):
 
 for i in range(2,n+1):
      print(parent[i])
+'''
