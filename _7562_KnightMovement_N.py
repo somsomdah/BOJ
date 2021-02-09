aac5bc258f620c09ce5e1499527fe8235705e222
somsomdah <somdah98@gmail.com>
2021-02-09 20:31:08 +0900

    210209

--- _7562_KnightMovement_N.py
+++ _7562_KnightMovement_N.py
@@ -2,33 +2,28 @@ dx=[1,1,-1,-1,2,2,-2,-2]
 dy=[2,-2,2,-2,1,-1,1,-1]
 
 
-def dfs(x,y):
-     global count
-     checklist[x][y]=1
-     count+=1
-     
-     if x==dest_x and y==dest_y:
-          return
-     else:
-          for i in range(8):
-               xx=x+dx[i]
-               yy=y+dy[i]
-
-               if 0<=xx<l and 0<=yy<l and checklist[xx][yy]==0:
-                    dfs(xx,yy)
-
-               
-     
-
 t=int(input())
 
 for _ in range(t):
      l=int(input())
      start_x,start_y=map(int,input().split())
      dest_x,dest_y=map(int,input().split())
-     checklist=[[0]*l for _ in range(l)]
-     count=0
 
-     dfs(start_x,start_y)
+     
+     dist=[[0]*l for _ in range(l)]
+     queue=[(start_x,start_y)]
 
-     print(count)
+     while queue:
+          tmp_x,tmp_y=queue.pop(0)
+          if tmp_x==dest_x and tmp_y==dest_y:
+               break
+          for i in range(8):
+               x=tmp_x+dx[i]
+               y=tmp_y+dy[i]
+
+               if 0<=x<l and 0<=y<l and dist[x][y]==0:
+                    dist[x][y]=1
+                    queue.append((x,y))
+                    dist[x][y]=dist[tmp_x][tmp_y]+1
+                    
+     print(dist[dest_x][dest_y])
