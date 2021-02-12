9eaae44bb97a3e94255fb51b9d5b465f70df9645
somsomdah <somdah98@gmail.com>
2021-02-12 22:39:55 +0900

    210212

--- _1197_MinimumSpanningTree.py
+++ _1197_MinimumSpanningTree.py
@@ -1,52 +1,50 @@
-import sys
-
-p,q=map(int,sys.stdin.readline().split())
-
-adj_list=[[] for _ in range(p+1)]
 
 
-for _ in range(q):
-     v1,v2,w=map(int,sys.stdin.readline().split())
-     adj_list[v1].append((v2,w))
-     adj_list[v2].append((v1,w))
+nv,ne=map(int,input().split()) # 정점 갯수, 간선 갯수
+edges=[tuple(map(int,input().split())) for _ in range(ne)]
+parent=list(range(nv+1)) # 선택된 노드가 없으니까 부모 노드는 자기 자신
 
-
-for i in range(1,p+1):
-     adj_list[p].sort(key=lambda x:x[1]) # weight가 작은 순으로 정렬
-
-
-S=[1] #selected
-U=[i for i in range(2,p+1)] #unselected
 total=0
 
-while U:
-     v,w=0,sys.maxsize
-     v_s=0
+def get_parent(v):
+     if parent[v] == v:
+          return v
+     else:
+          return get_parent(parent[v])
+
+
+
+def select_edge(edge):
+     global total
+     v1,v2,c=edge
      
-     for v1 in S:
-          if adj_list[v1]:
-               v2,w2=adj_list[v1][0]
-               if w2<w:
-                    v,w=v2,w2
-                    v_s=v1
+     a=get_parent(v1)
+     b=get_parent(v2)
 
-     if v not in S and v in U:
-          S.append(v)
-          U.remove(v)
+     # 더 작은 쪽이 부모 노드
+     if a>b:     
+          parent[a]=b
+     else:
+          parent[b]=a
+          
+     total+=c
+     
 
-          adj_list[v_s].remove((v,w))
-          adj_list[v].remove((v_s,w))
+def is_connected(v1,v2):
+     if get_parent(v1)==get_parent(v2):
+          return True
+     else:
+          return False
 
-          total+=w
+
+edges.sort(key=lambda x:x[2]) # 가중치 기준 오름차순 정렬
+
+for edge in edges: # 가중치가 작은 순
+     v1,v2,c=edge
+     if not is_connected(v1,v2): # MST에서 현재 두 노드가 연결되어 있지 않다면 (사이클 방지)
+          select_edge(edge) # 해당 간선 선택
 
 
 print(total)
-      
           
           
-     
-
-               
-          
-               
-     
